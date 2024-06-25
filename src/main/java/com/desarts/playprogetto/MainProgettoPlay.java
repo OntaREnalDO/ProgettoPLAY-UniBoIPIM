6e788a554f2b7873e7b412dc1ff5d23e7e39c91a
FabioSavio <fabio.savioli@studio.unibo.it>
2024-06-25 15:17:01 +0200

    aggiunta icona progetto

--- src/main/java/com/desarts/playprogetto/MainProgettoPlay.java
+++ src/main/java/com/desarts/playprogetto/MainProgettoPlay.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,8 @@ public class MainProgettoPlay extends Application {
     public void start(Stage stage) {
         MainProgettoPlay.primaryStage = stage;
         showWelcomeScene();  // Mostra la scena di benvenuto all'avvio
+        Image icon = new Image("com/desarts/playprogetto/25521.jpg");
+        primaryStage.getIcons().add(icon);
         primaryStage.setTitle("Progetto Play by DesArts");
         primaryStage.show();
     }

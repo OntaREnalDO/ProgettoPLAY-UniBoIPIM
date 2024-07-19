f9b71e5f0bce8203e6da912d1b252329cbba283f
AlbeZiro <145215365+AlbeZiro@users.noreply.github.com>
2024-07-19 17:00:26 +0200

    Update MainProgettoPlay.java

--- src/main/java/com/desarts/playprogetto/MainProgettoPlay.java
+++ src/main/java/com/desarts/playprogetto/MainProgettoPlay.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 
 public class MainProgettoPlay extends Application {
 
-    private static Stage primaryStage;  // Lo stage principale dell'applicazione
+    private static Stage primaryStage; 
     private static HomeController homeController;
 
 
@@ -21,10 +21,10 @@ public class MainProgettoPlay extends Application {
     @Override
     public void start(Stage stage) {
         MainProgettoPlay.primaryStage = stage;
-        showWelcomeScene();  // Mostra la scena di benvenuto all'avvio
+        showWelcomeScene(); 
         Image icon = new Image("com/desarts/playprogetto/immagini/25521.jpg");
-        primaryStage.getIcons().add(icon);   // Aggiunge l'icona in alto a sinistra nella schermata
-        primaryStage.setResizable(false);    // Non ti permette di ingrandire la schermata
+        primaryStage.getIcons().add(icon);   
+        primaryStage.setResizable(false);  
         primaryStage.setTitle("Progetto Play by DesArts");
         primaryStage.show();
     }
@@ -33,7 +33,6 @@ public class MainProgettoPlay extends Application {
         launch(args);
     }
 
-    // Metodo per visualizzare la scena di benvenuto
     public static void showWelcomeScene() {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(MainProgettoPlay.class.getResource( "welcome.fxml")));
@@ -43,7 +42,6 @@ public class MainProgettoPlay extends Application {
         }
     }
 
-    // Metodo per visualizzare la scena di login
     public static void showLoginScene() {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(MainProgettoPlay.class.getResource("login.fxml")));
@@ -54,7 +52,6 @@ public class MainProgettoPlay extends Application {
     }
 
 
-        // Metodo per visualizzare la scena delle impostazioni
         public static void showImpostazioniScene() {
             try {
                 Parent root = FXMLLoader.load(Objects.requireNonNull(MainProgettoPlay.class.getResource("impostazioni.fxml")));
@@ -65,7 +62,6 @@ public class MainProgettoPlay extends Application {
         }
 
 
-    // Metodo per visualizzare la scena di registrazione
     public static void showRegisterScene() {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(MainProgettoPlay.class.getResource("register.fxml")));
@@ -76,17 +72,14 @@ public class MainProgettoPlay extends Application {
     }
 
 
-    // Metodo per visualizzare la scena home
     public static void showHomeScene() {
         try {
             FXMLLoader loader = new FXMLLoader(MainProgettoPlay.class.getResource("home.fxml"));
             Parent root = loader.load();
 
-            // Ottieni il controller della scena home
             homeController = loader.getController();
             homeController.setStage(primaryStage);
-            homeController.updateProgressBars(); // Aggiorna le progress bar ogni volta che viene mostrata la home
-
+            homeController.updateProgressBars(); 
             primaryStage.setScene(new Scene(root));
             primaryStage.setTitle("PLAY");
             primaryStage.show();
